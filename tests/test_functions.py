62b256c6ebab90330abb99824bd895c4a1c9e871
Robert Frießleben <robert.friessleben@gmail.com>
2022-11-30 10:03:30 +0100

    Create Phone Number

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,1 +1,7 @@
+from solutions.create_phone_number import create_phone_number
 import codewars_test as test
+
+
+def test_create_phone_number():
+    test.assert_equals(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+
