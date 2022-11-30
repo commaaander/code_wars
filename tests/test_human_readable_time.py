6ed71a564280aae9ff1776e166a17266aa960eca
Robert Frießleben <robert.friessleben@gmail.com>
2022-11-30 19:41:28 +0100

    Human Readable Time

---
+++ tests/test_human_readable_time.py
@@ -0,0 +1,10 @@
+import _codewars_test as test
+from solutions.human_readable_time import make_readable
+
+
+def test_make_readable():
+    test.assert_equals(make_readable(0), "00:00:00")
+    test.assert_equals(make_readable(5), "00:00:05")
+    test.assert_equals(make_readable(60), "00:01:00")
+    test.assert_equals(make_readable(86399), "23:59:59")
+    test.assert_equals(make_readable(359999), "99:59:59")
