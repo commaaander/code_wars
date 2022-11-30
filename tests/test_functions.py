b1a55419387e4d6cb4ac5bf0d6cc9a32424730a9
Robert Frießleben <robert.friessleben@gmail.com>
2022-11-30 13:07:47 +0100

    Find the next perfect square

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,6 +2,7 @@ from solutions.create_phone_number import create_phone_number
 from solutions.take_a_ten_minutes_walk import is_valid_walk
 from solutions.highest_and_lowest import high_and_low
 from solutions.complementary_dna import DNA_strand
+from solutions.find_the_next_perfect_square import find_next_square
 import codewars_test as test
 
 
@@ -25,3 +26,12 @@ def test_DNA_strand():
     test.assert_equals(DNA_strand("AAAA"), "TTTT", "String AAAA is")
     test.assert_equals(DNA_strand("ATTGC"), "TAACG", "String ATTGC is")
     test.assert_equals(DNA_strand("GTAT"), "CATA", "String GTAT is")
+
+
+def test_find_the_next_perfect_square():
+    test.assert_equals(find_next_square(121), 144, "Wrong output for 121")
+    test.assert_equals(find_next_square(625), 676, "Wrong output for 625")
+    test.assert_equals(find_next_square(319225), 320356, "Wrong output for 319225")
+    test.assert_equals(find_next_square(15241383936), 15241630849, "Wrong output for 15241383936")
+    test.assert_equals(find_next_square(155), -1, "Wrong output for 155")
+    test.assert_equals(find_next_square(342786627), -1, "Wrong output for 342786627")
